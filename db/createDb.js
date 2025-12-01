8c0829edba25d26567434fc313c26475ab0c497b
Mathias-Maier <mathiasmr2002@gmail.com>
2025-12-01 10:45:15 +0100

    updated DB

--- db/createDb.js
+++ db/createDb.js
@@ -6,22 +6,31 @@ console.log('Recreating database...');
 const db = await connect();
 
 console.log('Dropping tables...');
-await db.query('drop table if exists tracks');
+await db.query('drop table if exists genres, songs');
 console.log('All tables dropped.');
 
 console.log('Recreating tables...');
 await db.query(`
-    create table tracks (
-        track_id bigint primary key,
-	    title text not null,
-	    artist text not null,
-	    duration int not null
+    create table genres (
+		name				text,
+        genre_id 			bigint primary key,
+	    text 				text
+    )
+`);
+
+await db.query(`
+    create table songs (
+        track_id 		bigint primary key,
+	    title 			text not null,
+	    artist 			text not null,
+		genre_id 		bigint references genre,
+	    duration_ms 	bigint not null
     )
 `);
 console.log('Tables recreated.');
 
 console.log('Importing data from CSV files...');
-await upload(db, 'db/short-tracks.csv', `
+await upload(db, 'db/genres.csv', `
 	copy tracks (track_id, title, artist, duration)
 	from stdin
 	with csv header`);
