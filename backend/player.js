4dfb62751a9f91484e447f0351dba31c07089f22
Mathias-Maier <mathiasmr2002@gmail.com>
2025-12-03 10:28:36 +0100

    Backend to frontend

--- backend/player.js
+++ backend/player.js
@@ -31,4 +31,3 @@ function trackElapsedTimes() {
 }
 
 trackElapsedTimes();
-
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,17 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { connect } from '../db/connect.js';
-import { play } from './player.js'; 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const db = await connect();
-const tracks = await loadTracks();
-const currentTracks = new Map(); // maps partyCode to index in tracks
 const parties = new Map(); // maps partyCode to { name: partyName, createdAt: Date }
 
 const port = process.env.PORT || 3003;
 const server = express();
 
 // --- Middleware ---
-server.use(express.static('frontend'));
+server.use(express.static('frontend', {index:'forside/index.html'}));
 server.use(express.json());
 server.use(onEachRequest);
 
@@ -41,12 +38,6 @@ server.get('/api/party/:partyCode', (req, res) => {
   res.json({ partyCode, partyName: party.name });
 });
 
-// Get current track
-server.get('/api/party/:partyCode/currentTrack', onGetCurrentTrackAtParty);
-
-// Fallback for frontend routing
-server.get(/\/[a-zA-Z0-9-_/]+/, onFallback);
-
 // --- Start server ---
 server.listen(port, onServerReady);
 
@@ -54,11 +45,6 @@ server.listen(port, onServerReady);
 function onEachRequest(req, res, next) {
   console.log(new Date(), req.method, req.url);
   next();
-}
-
-// Serve requested file if exists, else fallback to index.html
-async function onFallback(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 }
 
 function onServerReady() {
@@ -69,28 +55,3 @@ async function loadTracks() {
   const dbResult = await db.query(`SELECT * FROM songs`);
   return dbResult.rows;
 }
-
-async function onGetCurrentTrackAtParty(req, res) {
-  const partyCode = req.params.partyCode;
-  let trackIndex = currentTracks.get(partyCode);
-
-  if (trackIndex === undefined) {
-    trackIndex = pickNextTrackFor(partyCode);
-  }
-
-  const track = tracks[trackIndex];
-  res.json(track);
-}
-
-function pickNextTrackFor(partyCode) {
-  const trackIndex = Math.floor(Math.random() * tracks.length);
-  currentTracks.set(partyCode, trackIndex);
-
-  const track = tracks[trackIndex];
-  play(partyCode, track.track_id, track.duration_ms, Date.now(), () =>
-    currentTracks.delete(partyCode)
-  );
-
-  return trackIndex;
-}
-
